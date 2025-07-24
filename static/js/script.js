d05b7909462e9f1a66be4e150c9fcfcec4070dbc
Zaineb Brh <zainebbourhou123@gmail.com>
2025-07-24 17:49:21 +0200

    ajouter un script pour le formulaire de booking

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,32 @@
 
-        // animation sections
-        AOS.init({
-            duration: 1000, // durée de l’animation (en ms)
-            once: true      // anime une seule fois
-        });
+// animation sections
+AOS.init({
+    duration: 1000, // durée de l’animation (en ms)
+    once: true      // anime une seule fois
+});
 
         
-        // envoie de message
-        function sendEmail(event) {
-            event.preventDefault();
+// envoie de message
+function sendEmail(event) {
+    event.preventDefault();
 
-            const name = document.getElementById("name").value;
-            const email = document.getElementById("email_address").value;
-            const message = document.getElementById("message").value;
-            
-            const subject = encodeURIComponent("Contact form : " + name);
-            const body = encodeURIComponent("Name : " + name + "\nEmail : " + email + "\n\nMessage :\n" + message);
-            window.location.href = `mailto:Aloumavoyages1@gmail.com?subject=${subject}&body=${body}`;
-        }
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email_address").value;
+    const message = document.getElementById("message").value;            
+    const subject = encodeURIComponent("Contact form : " + name);
+    const body = encodeURIComponent("Name : " + name + "\nEmail : " + email + "\n\nMessage :\n" + message);
+    window.location.href = `mailto:Aloumavoyages1@gmail.com?subject=${subject}&body=${body}`;
+}
+
+// booking
+function sendEmail(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email_address").value;
+    const message = document.getElementById("message").value;
+        
+    const subject = encodeURIComponent("Contact form : " + name);
+    const body = encodeURIComponent("Name : " + name + "\nEmail : " + email + "\n\nMessage :\n" + message);
+    window.location.href = `mailto:Aloumavoyages1@gmail.com?subject=${subject}&body=${body}`;
+};
